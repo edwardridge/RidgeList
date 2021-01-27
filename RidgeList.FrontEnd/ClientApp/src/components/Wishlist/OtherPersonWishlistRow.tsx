0aaed1ff096108d32d6ab6ba8e03fec4de96cb74
Edward Ridge <edward.ridge@umusic.com>
2021-01-27 08:48:57 +0000

    wip

--- RidgeList.FrontEnd/ClientApp/src/components/Wishlist/OtherPersonWishlistRow.tsx
+++ RidgeList.FrontEnd/ClientApp/src/components/Wishlist/OtherPersonWishlistRow.tsx
@@ -24,33 +24,42 @@ export const OtherPersonWishlistRow = (props : OtherPersonWishlistRowProps) => {
             .unclaimPresent(props.wishlistId, presentId);
         props.setWishlist(wishlist);
     }
+    let getPresentIdeas = () => {
+        if(props.wishlistPerson.presentIdeas?.length === 0){
+            return <div>They haven't added any gift ideas yet!</div>
+        }
+        return props.wishlistPerson.presentIdeas?.map(s => {
+            let claimed = s.claimerEmail !== null && s.claimerEmail !== '';
+            let claimedByYou = s.claimerEmail === props.loggedInEmail;
+            let unclaim = claimedByYou ? <button className='btn btn-outline-danger w-100'
+                                                 onClick={() => unclaimPresentClick(s.id)}>Unclaim</button> : null;
+            let claimerText = claimed ? `- claimed by ${claimedByYou ? "you" : s.claimerName}` : '';
+            let claimSection = s.claimerName ?
+                <>{unclaim}</> :
+                <button className='btn btn-outline-success w-100'
+                        onClick={() => claimPresentClick(s.id)}>Claim</button>;
 
+            let classes = `mt-1 ml-0 mr-0 row ${claimed ? 'claimed' : ''}`;
+            return (
+                <tr key={s.id} className={classes}>
+                    <td className='col-8 col-md-10' cypress-name={`${s.id}-description`}>
+                        <Linkify>{s.description} {claimerText}</Linkify></td>
+                    <td className='col-4 col-md-2 text-right' cypress-name={`${s.id}-buttons`}>{claimSection}</td>
+                </tr>
+            )
+        });
+    }
+    
     return (
         <div className='wishlistSummaryItem mb-3' key={`${props.wishlistPerson.email}`}>
             <div>
                 <span className='d-inline-block col-12 text-center'>
-                    {props.wishlistPerson.name} - {props.wishlistPerson.giftee ? 'IS GIFTEE' : 'NOT'}
+                    {props.wishlistPerson.name}
                     <span className='emailDetails'> ({props.wishlistPerson.email})</span>
                 </span>
             </div>
             <table className='table'>
-                {props.wishlistPerson.presentIdeas?.map(s => {
-                    let claimed = s.claimerEmail !== null && s.claimerEmail !== '';
-                    let claimedByYou = s.claimerEmail === props.loggedInEmail;
-                    let unclaim = claimedByYou ? <button className='btn btn-outline-danger w-100' onClick={() => unclaimPresentClick(s.id)}>Unclaim</button> : null;
-                    let claimerText = claimed ? `- claimed by ${claimedByYou ? "you" : s.claimerName}` : '';
-                    let claimSection = s.claimerName ?
-                        <>{unclaim}</> :
-                        <button className='btn btn-outline-success w-100' onClick={() => claimPresentClick(s.id)}>Claim</button>;
-
-                    let classes = `mt-1 ml-0 mr-0 row ${claimed ? 'claimed' : ''}`;
-                    return (
-                        <tr key={s.id} className={classes}>
-                            <td className='col-8 col-md-10' cypress-name={`${s.id}-description`}><Linkify>{s.description} {claimerText}</Linkify></td>
-                            <td className='col-4 col-md-2 text-right' cypress-name={`${s.id}-buttons`}>{claimSection}</td>
-                        </tr>
-                    ) 
-                })}
+                {getPresentIdeas()}
             </table>
         </div>
     )

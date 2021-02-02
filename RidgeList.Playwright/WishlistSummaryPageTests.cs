d28ef3a43ce275a28351cd58a0e7cff2a53878e1
Edward Ridge <edward.ridge@umusic.com>
2021-02-02 14:11:26 +0000

    asd

--- RidgeList.Playwright/WishlistSummaryPageTests.cs
+++ RidgeList.Playwright/WishlistSummaryPageTests.cs
@@ -1,27 +1,27 @@
-// using System;
-// using System.Threading.Tasks;
-// using FluentAssertions;
-// using NUnit.Framework;
-//
-// namespace RidgeList.Playwright
-// {
-//     public class WishlistSummaryPageTests : WishlistTestBase
-//     {
-//         [SetUp]
-//         public async Task SetupThis()
-//         {
-//             await loginPage.LoginWithCookie("test@testwishlist.com", "Test", baseUrl, baseUrl);
-//         }
-//
-//         [Test]
-//         public async Task CanCreateNewWishlist()
-//         {
-//             var rand = new Random().Next(0, 10000);
-//             var name = $"[Test] From Cypress {rand}";
-//             await page.ClickAsync(Cy.Name("CreateNewWishlist"));
-//             await page.TypeAsync(Cy.Name("NameOfWishlist"), name);
-//             await page.ClickAsync(Cy.Name("Create"));
-//             page.Url.Should().Contain("/wishlist");
-//         }
-//     }
-// }
+using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace RidgeList.Playwright
+{
+    public class WishlistSummaryPageTests : WishlistTestBase
+    {
+        [SetUp]
+        public async Task SetupThis()
+        {
+            await loginPage.LoginWithCookie("test@testwishlist.com", "Test", baseUrl, baseUrl);
+        }
+
+        [Test]
+        public async Task CanCreateNewWishlist()
+        {
+            var rand = new Random().Next(0, 10000);
+            var name = $"[Test] From Cypress {rand}";
+            await page.ClickAsync(Cy.Name("CreateNewWishlist"));
+            await page.TypeAsync(Cy.Name("NameOfWishlist"), name);
+            await page.ClickAsync(Cy.Name("Create"));
+            page.Url.Should().Contain("/wishlist");
+        }
+    }
+}
--- RidgeList.Playwright/WishlistTests.cs
+++ RidgeList.Playwright/WishlistTests.cs
@@ -1,69 +1,69 @@
-// using System;
-// using System.Threading.Tasks;
-// using FluentAssertions;
-// using NUnit.Framework;
-//
-// namespace RidgeList.Playwright
-// {
-//     public class WishlistTests : WishlistTestBase
-//     {
-//         [SetUp]
-//         public async Task SetupThis()
-//         {
-//             await loginPage.LoginWithCookie("test@testwishlist.com", "Test", baseUrl, baseUrl);
-//         }
-//         
-//         [Test]
-//         public async Task AllowsNewPeopleToBeAdded()
-//         {
-//             await PlaywrightHelpers.CreateWishlist("AllowsNewPeopleToBeAdded", page);
-//             var wishlistPage = new WishlistPageObject(page);
-//             await wishlistPage.AddNewPerson("ed@ed.com", "Ed");
-//         }
-//         
-//         [Test]
-//         public async Task DoesntAllowDuplicateNamesToBeAdded()
-//         {
-//             await PlaywrightHelpers.CreateWishlist("DoesntAllowDuplicateNamesToBeAdded", page);
-//             var wishlistPage = new WishlistPageObject(page);
-//             await wishlistPage.AddNewPerson("Ed", "ed@ed.com");
-//             
-//             await wishlistPage.ClickAddNewPerson();
-//             await wishlistPage.EnterNameAndEmail("Ed", "ed@ed.com");
-//             
-//             var createNewPersonIsEnabled = await wishlistPage.CreateNewPersonIsEnabled();
-//             createNewPersonIsEnabled.Should().BeFalse();
-//             
-//             await wishlistPage.EnterNameAndEmail("Ed", "ed2@ed.com");
-//             createNewPersonIsEnabled = await wishlistPage.CreateNewPersonIsEnabled();
-//             createNewPersonIsEnabled.Should().BeTrue();
-//         }
-//         
-//         [Test]
-//         public async Task AllowsYouToAddItem()
-//         {
-//             await PlaywrightHelpers.CreateWishlist("AllowsYouToAddItem", page);
-//             var wishlistPage = new WishlistPageObject(page);
-//             await wishlistPage.AddNewPerson("Ed", "ed@ed.com");
-//
-//             await wishlistPage.AddItem("New present idea");
-//         }
-//         
-//         [Test]
-//         public async Task AllowsYouToClaimAndUnclaim()
-//         {
-//             var rand = Guid.NewGuid().ToString();
-//             var nameOfWishlist = "AllowsYouToClaimAndUnclaim " + rand;
-//             await PlaywrightHelpers.CreateWishlist(nameOfWishlist, page);
-//             var wishlistPage = new WishlistPageObject(page);
-//             await wishlistPage.AddNewPerson("New", "new@new.com");
-//             await wishlistPage.AddItem("New present idea");
-//
-//             await loginPage.LoginWithCookie("new@new.com", "New", baseUrl, baseUrl);
-//             await page.GoToAsync(baseUrl);
-//             await page.ClickAsync("text=" + nameOfWishlist);
-//
-//             await page.ClickAsync("td:right-of(:text('New present idea'))");
-//         }
-//     }
-// }
+using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace RidgeList.Playwright
+{
+    public class WishlistTests : WishlistTestBase
+    {
+        [SetUp]
+        public async Task SetupThis()
+        {
+            await loginPage.LoginWithCookie("test@testwishlist.com", "Test", baseUrl, baseUrl);
+        }
+        
+        [Test]
+        public async Task AllowsNewPeopleToBeAdded()
+        {
+            await PlaywrightHelpers.CreateWishlist("AllowsNewPeopleToBeAdded", page);
+            var wishlistPage = new WishlistPageObject(page);
+            await wishlistPage.AddNewPerson("ed@ed.com", "Ed");
+        }
+        
+        [Test]
+        public async Task DoesntAllowDuplicateNamesToBeAdded()
+        {
+            await PlaywrightHelpers.CreateWishlist("DoesntAllowDuplicateNamesToBeAdded", page);
+            var wishlistPage = new WishlistPageObject(page);
+            await wishlistPage.AddNewPerson("Ed", "ed@ed.com");
+            
+            await wishlistPage.ClickAddNewPerson();
+            await wishlistPage.EnterNameAndEmail("Ed", "ed@ed.com");
+            
+            var createNewPersonIsEnabled = await wishlistPage.CreateNewPersonIsEnabled();
+            createNewPersonIsEnabled.Should().BeFalse();
+            
+            await wishlistPage.EnterNameAndEmail("Ed", "ed2@ed.com");
+            createNewPersonIsEnabled = await wishlistPage.CreateNewPersonIsEnabled();
+            createNewPersonIsEnabled.Should().BeTrue();
+        }
+        
+        [Test]
+        public async Task AllowsYouToAddItem()
+        {
+            await PlaywrightHelpers.CreateWishlist("AllowsYouToAddItem", page);
+            var wishlistPage = new WishlistPageObject(page);
+            await wishlistPage.AddNewPerson("Ed", "ed@ed.com");
+
+            await wishlistPage.AddItem("New present idea");
+        }
+        
+        [Test]
+        public async Task AllowsYouToClaimAndUnclaim()
+        {
+            var rand = Guid.NewGuid().ToString();
+            var nameOfWishlist = "AllowsYouToClaimAndUnclaim " + rand;
+            await PlaywrightHelpers.CreateWishlist(nameOfWishlist, page);
+            var wishlistPage = new WishlistPageObject(page);
+            await wishlistPage.AddNewPerson("New", "new@new.com");
+            await wishlistPage.AddItem("New present idea");
+
+            await loginPage.LoginWithCookie("new@new.com", "New", baseUrl, baseUrl);
+            await page.GoToAsync(baseUrl);
+            await page.ClickAsync("text=" + nameOfWishlist);
+
+            await page.ClickAsync("td:right-of(:text('New present idea'))");
+        }
+    }
+}

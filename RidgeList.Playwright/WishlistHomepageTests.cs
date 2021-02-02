f5b12fadbf71156f7db6478372edf91f51b8dcef
Edward Ridge <edward.ridge@umusic.com>
2021-02-02 14:09:32 +0000

    Update WishlistHomepageTests.cs

--- RidgeList.Playwright/WishlistHomepageTests.cs
+++ RidgeList.Playwright/WishlistHomepageTests.cs
@@ -9,31 +9,23 @@ namespace RidgeList.Playwright
     [TestFixture]
     public class WishlistHomepageTests : WishlistTestBase
     {
-        // public const string baseUrl = "https://ridgelist-ci-gfhqqojama-nw.a.run.app";
-        
         [Test]
         public async Task LoginWorks()
         {
-            // var playwright = await PlaywrightSharp.Playwright.CreateAsync();
-            // var browser = await playwright.Chromium.LaunchAsync();
-            // var page = await browser.NewPageAsync();
-            // await page.GoToAsync(baseUrl);
-            // await page.Context.ClearCookiesAsync();
-            // var loginPage = new LoginPageObject(page);
             await loginPage.LoginUsingFormWithTestAccount();
 
             page.Url.Should().Contain("/wishlists");
         }
         
-        // [Test]
-        // public async Task NoCookieRedirectsBackToHomepage()
-        // {
-        //     await loginPage.LoginWithCookie("test@testwishlist.com", "Test", baseUrl, baseUrl);
-        //
-        //     await page.Context.ClearCookiesAsync();
-        //     await page.GoToAsync(baseUrl);
-        //     
-        //     page.Url.Should().NotContain("/wishlists");
-        // }
+        [Test]
+        public async Task NoCookieRedirectsBackToHomepage()
+        {
+            await loginPage.LoginWithCookie("test@testwishlist.com", "Test", baseUrl, baseUrl);
+        
+            await page.Context.ClearCookiesAsync();
+            await page.GoToAsync(baseUrl);
+            
+            page.Url.Should().NotContain("/wishlists");
+        }
     }
 }

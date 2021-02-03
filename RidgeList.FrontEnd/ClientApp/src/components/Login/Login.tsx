e342187c0a577d9a6d2a2b18c5b478a62505c610
Ed <edwardridge@gmail.com>
2021-02-03 16:18:16 +0000

    WIp

--- RidgeList.FrontEnd/ClientApp/src/components/Login/Login.tsx
+++ RidgeList.FrontEnd/ClientApp/src/components/Login/Login.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Button } from "react-bootstrap";
 import { useHistory } from "react-router-dom";
 import {useSetLogin, useGetLogin} from "../useLogin";
 import "./Login.css"
@@ -22,16 +23,16 @@ export const Login : React.FC<any> = (props) => {
     }
      
     return (
-        <div className='loginWrapper '>
+        <div className='loginWrapper'>
             <h5>Welcome to RidgeList! To login, please enter your name and email address - note that the wishlists are linked to your email address.</h5>
             <div className='form-group mt-lg-4 input-group-lg'>
-                {/*<label htmlFor="NameLogin">Name</label>*/}
                 <input type="text" className='form-control' id='NameLogin' cypress-name="NameLogin" placeholder="Your name..." onChange={(e) => setLoginName(e.target.value)}/>
             </div>
             <div className='form-group input-group-lg'>
-                {/*<label htmlFor="EmailLogin">Email</label>*/}
                 <input type="text" className='form-control' id='EmailLogin' cypress-name="EmailLogin" placeholder="Your email address..." onChange={(e) => setLoginEmail(e.target.value)}/>
             </div>
-            <button className="btn btn-success" onClick={loginClicked} cypress-name="LoginButton">Login</button>
+            <Button size='lg' variant="success" cypress-name='LoginButton' onClick={loginClicked}>
+                Login
+            </Button>
         </div>)
 }
